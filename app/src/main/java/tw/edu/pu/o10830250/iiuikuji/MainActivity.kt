749660f1d3137fe324566138d3310f7bb85b6385
puikefarn <wochaoniba1234@gmail.com>
2021-05-17 14:40:13 +0800

    0517

---
+++ app/src/main/java/tw/edu/pu/o10830250/iiuikuji/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.o10830250.iiuikuji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
